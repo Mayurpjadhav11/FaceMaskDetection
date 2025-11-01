fdb671051ae5c205072353bd690e39f1c774188a
Mayurpjadhav11 <168913746+Mayurpjadhav11@users.noreply.github.com>
2025-11-01 18:04:23 +0100

    Converted myapp from submodule to normal folder

---
+++ myapp/src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Kau!</h1>
+      <p>I’m learning React step by step.</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
